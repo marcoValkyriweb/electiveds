37bfffbe75c7d53e5996a31e9b7b6e13a615f45c
Sean Mphanga <215004019@mycput.ac.za>
2017-11-07 12:12:32 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/ExtremeCoders/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.ExtremeCoders;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
