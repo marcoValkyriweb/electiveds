031fa23e613b3f90ecfcb197342a6021a250be71
marcoValkyriweb <34270069+marcoValkyriweb@users.noreply.github.com>
2019-08-27 22:28:33 +0200

    Array Chat
    Hey Senzo, these are the changes as we discussed.

--- SenzoRun.java
+++ SenzoRun.java
@@ -13,29 +13,28 @@ public class SenzoRun
 
     public void runLaptops()
     {
-        Laptop laptop1 = new Laptop(196545484,"Samsung", "Odyssey");
-        Laptop laptop2 = new Laptop(284795214,"Acer", "GK6AA");
-        Laptop laptop3 = new Laptop(489998997,"Lenovo", "G50");
-        Laptop laptop4 = new Laptop(489998997,"HP", "G6");
-
-        list.add(laptop1);
-        list.add(laptop2);
-        list.add(laptop3);
-        list.add(laptop4);
+        Laptop laptops[] = new Laptop[] {
+            new Laptop(196545484,"Samsung", "Odyssey"), 
+            new Laptop(284795214,"Acer", "GK6AA"),
+            new Laptop(489998997,"Lenovo", "G50"),
+            new Laptop(489998997,"HP", "G6"),
+        };
+        
+        for (Laptop laptop : laptops) {
+            list.add(laptop);    
+        }
 
         System.out.println("Number of laptops in the list: " + list.size());
 
-        set.add(laptop1);
-        set.add(laptop2);
-        set.add(laptop3);
-        set.add(laptop4);
+        for (Laptop laptop : laptops) {
+            set.add(laptop);
+        }
 
         System.out.println("Number of laptops in the set: " + set.size());
 
-        singlyLinkedList.addAtHead(laptop1);
-        singlyLinkedList.addAtHead(laptop2);
-        singlyLinkedList.addAtHead(laptop3);
-        singlyLinkedList.addAtHead(laptop4);
+        for (Laptop laptop : laptops) {
+            singlyLinkedList.addAtHead(laptop);
+        }
 
         singlyLinkedList.print();
     }
